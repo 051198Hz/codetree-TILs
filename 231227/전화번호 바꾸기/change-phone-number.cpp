8c68f98012b35078fac6385cef064bf4340a3160
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-27 17:39:11 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "stdio.h"
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b,c;
+    scanf("010-%d-%d",&b,&c);
+    cout<<"010-"<<c<<"-"<<b;
+    return 0;
+}
