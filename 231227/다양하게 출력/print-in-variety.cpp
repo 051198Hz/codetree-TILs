4604a1cc6a19aaa6c7128a6a248c4e2b1635345e
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-27 17:06:37 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a = "Total days in Year", b = "365", c = "Circumference rate", d = "3.1415926535";
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d;
+    return 0;
+}
