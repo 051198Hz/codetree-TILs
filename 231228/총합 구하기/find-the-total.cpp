9c67471ff20eabb0a9355ddb8744a99f9bbe9199
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-28 13:01:55 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/총합 구하기/find-the-total.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,sum = 0;
+    std::cin>>a>>b;
+    for(int i = a; i <= b; i++){
+        if( !(i%6) && (i%8) ){
+            sum+=i;
+        }
+    }
+    std::cout<<sum;
+    return 0;
+}
