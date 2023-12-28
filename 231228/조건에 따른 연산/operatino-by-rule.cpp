6de6be1b35bd16335341fbff5dd330345af52277
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-28 14:30:57 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231228/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin>>n;
+
+    for(int i = 0; ; i++){
+        if(n%2){
+            n*=3;
+            n+=1;
+        }else{
+            n*=2;
+            n+=2;
+        }
+        if( !(n<1000)) {
+            std::cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
