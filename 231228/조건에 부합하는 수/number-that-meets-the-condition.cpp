781c98c2c18c91fa2037d31e2f4fdae1972c1a37
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-28 13:45:55 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin>>a;
+
+    for(int i = 1; i<=a; i++){
+if( !( (i % 2 == 0 ) && ( i % 4 != 0)) &&
+    ! (((i/8)%2 )==0) &&
+    !( (i%7) < 4)  ){
+        std::cout<<i<<" ";
+    }
+
+    }
+
+
+
+    return 0;
+}
