7fe4a546a59f30516288de4ea2f42e07fad133f8
Yune gim <82448654+051198Hz@users.noreply.github.com>
2023-12-31 01:04:34 +0900

    Title: 별표 출력하기 13, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    
+    int n;
+    cin>>n;
+
+    vector<int> v;
+
+    if(n==1){
+        cout<<"*"<<"\n"<<"*";
+    }else{
+        for(int i = n; i > 1; i--){
+            v.push_back(i);
+        }
+    }
+
+    v.insert(v.begin()+1, 1);
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<v.at(i); j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+
+    for(int i = n-1; i >=0; i--){
+        for(int j = v.at(i) -1 ; j>=0; j--){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
