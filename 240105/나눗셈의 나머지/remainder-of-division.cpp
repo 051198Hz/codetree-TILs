6f1f556327f12b8856551881f46b94a8b2edfa58
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-01-05 01:26:41 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240105/나눗셈의 나머지/remainder-of-division.cpp
+++ 240105/나눗셈의 나머지/remainder-of-division.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b,cnt[12] = {0,}; cin>>a>>b;
+    int a,b,cnt[1001] = {0,}; cin>>a>>b;
     for(int i = 0; a>=1; i++){
         cnt[a%b]++;
         a = a / b;
     }
-    for(int i = 0; i<11; i++){
-        cnt[11] += cnt[i]*cnt[i];
+    for(int i = 0; i<1000; i++){
+        cnt[1000] += cnt[i]*cnt[i];
     }
-    cout<<cnt[11];
+    cout<<cnt[1000];
     return 0;
 }
