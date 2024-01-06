0b3f9bc895e29f1ee8e0e186437c742341c29fee
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-01-06 19:01:25 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240106/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+    int n; cin>>n;
+
+    int cnt = 0,sum = 0;
+
+    string strs[10];
+    for(int i = 0; i<n; i++){
+        cin>>strs[i];
+    }
+
+    char c; cin>>c;
+
+    for(int i = 0; i<n; i++){
+        if(strs[i][0]==c){
+            sum+=strs[i].size();
+            cnt++;
+        }
+    }
+    printf("%d %.2lf",cnt,(double)sum/(double)cnt);
+    return 0;
+}
